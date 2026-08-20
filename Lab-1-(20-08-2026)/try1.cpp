756cf00b1fda15ae7e030b82f63bbf87b292d253
Asem Hamid <197576625+as-em@users.noreply.github.com>
2026-08-20 14:44:05 +0600

    Update try1.cpp

--- Lab-1-(20-08-2026)/try1.cpp
+++ Lab-1-(20-08-2026)/try1.cpp
@@ -1,1 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// given intiger num , find even or odd
+
+int main()
+{
+
+    int num;
+    cin >> num;
+
+    if (num & 1)
+    {
+        cout << "Odd";
+    }
+    else
+    {
+        cout << "Even";
+    }
+    cout << endl;
+
+    {
+
+        bitset<8> bits1(num);
+        bits1[0] = 1;
+        // int newb = bits;
+
+        cout << (bits1.to_ullong());
+    }
+    return 0;
+}
